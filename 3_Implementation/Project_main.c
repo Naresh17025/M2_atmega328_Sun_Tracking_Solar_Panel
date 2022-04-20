a71e5bab97e95d504d62f65b31d83e7e1cf23269
Naresh17025 <101312396+Naresh17025@users.noreply.github.com>
2022-04-20 18:03:20 +0530

    Update and rename Sun Tracking Solar Pannel.c to Project_main.c

---
+++ 3_Implementation/Project_main.c
@@ -0,0 +1,19 @@
+/**
+ * @file project_main.c
+ * @author NARESH D (naresh.d.2018.ece@ritchennai.edu.in)
+ * @brief Project to SUN TRACKING SOLAR PANEL
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <Servo.h>
+
+Servo myservo;  
+int ldr1 = 4; 
+int ldr2 = 5;
+int val1;
+int val2;   
+int pos=90;
+

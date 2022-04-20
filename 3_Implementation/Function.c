cfb994e86a75e9848b5d44871310beb3e9a3b01e
Naresh17025 <101312396+Naresh17025@users.noreply.github.com>
2022-04-20 18:06:48 +0530

    Create Function.c

---
+++ 3_Implementation/Function.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void setup() 
+{
+  myservo.attach(11); 
+  Serial.begin(9600);
+  myservo.write(pos);   
+}
+void loop() 
+{
+  val1 = analogRead(ldr1);            
+  val2 = analogRead(ldr2);
+  val1 = map(val1, 0, 1023, 0, 180);     
+  val2 = map(val2, 0, 1023, 0, 180);     
+  if(val1 > (val2+50))
+  {    
+    if(pos<180)
+     pos=pos+1;
+    myservo.write(pos);
+    Serial.println("backward");         
+    delay(10);         
+  }
+  else if(val2 > (val1+50))
+  {
+    if(pos>0)
+     pos=pos-1;
+    myservo.write(pos);
+    Serial.println("forward");         
+    delay(10); 
+  }
+                     
+}

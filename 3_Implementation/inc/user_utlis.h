33e58f1219351b00539b81be1217e8814d247ad9
Naresh17025 <101312396+Naresh17025@users.noreply.github.com>
2022-04-20 19:01:25 +0530

    Create user_utlis.h

---
+++ 3_Implementation/inc/user_utlis.h
@@ -0,0 +1,24 @@
+#ifndef __USER_UTILS_H__
+#define __USER_UTILS_H__
+/**
+ * @file user_utils.h
+ * @author NARESH D (naresh.d.2018.ece@ritchennai.edu.in)
+ * @brief User defined utilities used across the project
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "project_config.h"
+#include <util/delay.h>
+
+/**
+ * @brief Function to generate delay in micro seconds
+ * 
+ * @return delay_time Delay in Micro seconds
+ * 
+ */
+void delay_ms(delay(10));
+
+#endif /* __USER_UTILS_H__ */
